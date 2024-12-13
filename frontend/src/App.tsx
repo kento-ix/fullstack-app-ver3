04b4a536cbf8c8ee376cbb2eaadde687dd4ca9c2
kento-ix <kentoix1105@gmail.com>
2024-12-13 01:13:13 -0800

    update frontend directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
-import Register from './auth/register';
-import Login from './auth/login';
 import Header from './common/header';
-import MyListing from './pages/listing'
+import MyListing from './pages/Listing'
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <Router>
         <Header/>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element={<Home />}/>
         <Route path="/mylisting" element={<MyListing />} />
       </Routes>
     </Router>
